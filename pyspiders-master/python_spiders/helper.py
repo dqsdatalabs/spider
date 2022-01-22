ee10497dd3cb83981a67d879f289b69b03b637f4
ahmed2299 <55079510+ahmed2299@users.noreply.github.com>
2022-01-22 14:59:01 +0200

    Update helper.py
    Co-Authored-By: mohossam589 <98216809+mohossam589@users.noreply.github.com>

--- pyspiders-master/python_spiders/helper.py
+++ pyspiders-master/python_spiders/helper.py
@@ -421,10 +421,7 @@ def get_amenities(description, Amenties_text, item_loader):
         x in description for x in Amenties['dishwasher']) else None
 
 
-
-
-
-
+    x=123
     item_loader.add_value("pets_allowed", pets_allowed)  # Boolean
     item_loader.add_value("furnished", furnished)  # Boolean
     item_loader.add_value("parking", parking)  # Boolean
